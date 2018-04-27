06e735ec3c84e00a9ea95d64fb5c7e0404dfe62f
Arya1957 <30673959+Arya1957@users.noreply.github.com>
2018-04-27 14:14:13 +0800

    Update index.js

--- demo5/static/index.js
+++ demo5/static/index.js
@@ -20,11 +20,8 @@ $btn.on('click', function (e) {
         return;  // 如果正在加载数据，就什么都不做
     }
     isLoading = true;
-<<<<<<< HEAD
     $btn.html('<img src="loading.gif"/>');
-=======
-    $btn.html('<img src="https://xiedaimala.com/s/36"/>');
->>>>>>> gh-pages
+
     $.ajax({
         url: '/loadmore',
         method: 'GET',
